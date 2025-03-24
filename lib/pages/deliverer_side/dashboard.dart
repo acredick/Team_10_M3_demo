fe72267f6b468c42c0cc01687f01f7e364382511
acredick <acredick@albany.edu>
2025-03-24 13:04:40 -0400

    limit # of accepted orders

--- lib/pages/deliverer_side/dashboard.dart
+++ lib/pages/deliverer_side/dashboard.dart
@@ -1,138 +1,166 @@
 import 'package:flutter/material.dart';
 import 'package:DormDash/widgets/bottom-nav-bar.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  bool isOrderAccepted = false; // Track if an order has been accepted
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          // Header Section
-          Container(
-            padding: EdgeInsets.all(16),
-            width: MediaQuery.of(context).size.width * 0.98,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Color(0xFF5B3184),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Row(
-
-    crossAxisAlignment: CrossAxisAlignment.start,
-
-    children: [
-      // Text Section
-      Expanded(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "HELLO",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
+            // Header Section
+            Container(
+              padding: EdgeInsets.all(16),
+              width: MediaQuery.of(context).size.width * 0.98,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Color(0xFF5B3184),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Text Section
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "HELLO",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "STUDENT NAME",
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 25,
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                        Text(
+                          "Earned Money",
+                          style: TextStyle(color: Colors.white70, fontSize: 12),
+                        ),
+                        Text(
+                          "\$000.000",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Map Placeholder
+                  Padding(
+                    padding: EdgeInsets.only(right: 30),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        height: MediaQuery.of(context).size.width * 0.3,
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        color: Colors.white,
+                        child: Image.asset(
+                          'assets/images/mapimage.png', // Replace with actual Google Maps widget
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 5),
-            Text(
-              "STUDENT NAME",
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 25,
+            // Filter Section
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Filter",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-            SizedBox(height: 40),
-            Text(
-              "Earned Money",
-              style: TextStyle(color: Colors.white70, fontSize: 12),
-            ),
-            Text(
-              "\$000.000",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            // Order List Section
+            Expanded(
+              child: ListView(
+                children: [
+                  OrderCard(
+                    image: 'assets/images/food1.png',
+                    pickup: 'Campus Center',
+                    dropoff: 'University Library',
+                    price: 5,
+                    isOrderAccepted: isOrderAccepted,
+                    onAccept: () {
+                      if (!isOrderAccepted) {
+                        setState(() {
+                          isOrderAccepted = true;
+                        });
+                      }
+                    },
+                  ),
+                  OrderCard(
+                    image: 'assets/images/food2.png',
+                    pickup: 'Campus Center',
+                    dropoff: 'University Library',
+                    price: 5,
+                    isOrderAccepted: isOrderAccepted,
+                    onAccept: () {
+                      if (!isOrderAccepted) {
+                        setState(() {
+                          isOrderAccepted = true;
+                        });
+                      }
+                    },
+                  ),
+                  OrderCard(
+                    image: 'assets/images/food3.png',
+                    pickup: 'Campus Center',
+                    dropoff: 'University Library',
+                    price: 5,
+                    isOrderAccepted: isOrderAccepted,
+                    onAccept: () {
+                      if (!isOrderAccepted) {
+                        setState(() {
+                          isOrderAccepted = true;
+                        });
+                      }
+                    },
+                  ),
+                ],
               ),
             ),
           ],
         ),
-
       ),
-                // Map Placeholder
-                Padding(
-                  padding: EdgeInsets.only(right: 30),
-                  child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    height: MediaQuery.of(context).size.width * 0.3,
-                    width: MediaQuery.of(context).size.width * 0.4,
-                    color: Colors.white,
-                    child: Image.asset(
-                      'assets/images/mapimage.png', // Replace with actual Google Maps widget 
-                      fit: BoxFit.cover,
-                     ),
-                  ),
-                ),
-                ),
-              ],
-            ),
-          ),
-          // Filter Section
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Filter",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          // Order List Section
-          Expanded(
-            child: ListView(
-              children: [
-                OrderCard(
-                  image: 'assets/images/food1.png',
-                  pickup: 'Campus Center',
-                  dropoff: 'University Library',
-                  price: 5,
-                ),
-                OrderCard(
-                  image: 'assets/images/food2.png',
-                  pickup: 'Campus Center',
-                  dropoff: 'University Library',
-                  price: 5,
-                ),
-                OrderCard(
-                  image: 'assets/images/food3.png',
-                  pickup: 'Campus Center',
-                  dropoff: 'University Library',
-                  price: 5,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-     ),
       // Bottom Navigation Bar
       bottomNavigationBar: CustomBottomNavigationBar(
-        selectedIndex: 0,          
-        userType: "deliverer",     
-        onItemTapped: (index) {},  
-       ),
-     );
-   }
+        selectedIndex: 0,
+        userType: "deliverer",
+        onItemTapped: (index) {},
+      ),
+    );
+  }
 }
 
 class OrderCard extends StatelessWidget {
@@ -140,12 +168,16 @@ class OrderCard extends StatelessWidget {
   final String pickup;
   final String dropoff;
   final int price;
+  final bool isOrderAccepted;
+  final VoidCallback onAccept;
 
   OrderCard({
     required this.image,
     required this.pickup,
     required this.dropoff,
     required this.price,
+    required this.isOrderAccepted,
+    required this.onAccept,
   });
 
   @override
@@ -176,8 +208,7 @@ class OrderCard extends StatelessWidget {
                   children: [
                     Text(
                       "Pickup Point",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
                     ),
                     Row(
                       children: [
@@ -189,8 +220,7 @@ class OrderCard extends StatelessWidget {
                     SizedBox(height: 5),
                     Text(
                       "Pick-out Point",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
                     ),
                     Row(
                       children: [
@@ -202,8 +232,7 @@ class OrderCard extends StatelessWidget {
                     SizedBox(height: 5),
                     Text(
                       "Earn: \$$price",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
                     ),
                   ],
                 ),
@@ -212,12 +241,12 @@ class OrderCard extends StatelessWidget {
             Padding(
               padding: EdgeInsets.only(right: 12),
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: isOrderAccepted ? null : onAccept,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xFFDCB347), // Gold button color
                   foregroundColor: Colors.white,
                 ),
-                child: Text("Accept"),
+                child: Text(isOrderAccepted ? "Order Accepted" : "Accept"),
               ),
             ),
           ],
