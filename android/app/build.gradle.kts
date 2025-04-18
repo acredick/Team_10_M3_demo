5e311440f9315ce3c6ae57ac5ce38c7cbdb75502
acredick <acredick@albany.edu>
2025-04-17 20:22:13 -0400

    app launch setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.team_10_m3_demo"
+    namespace = "com.github.acredick"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.team_10_m3_demo"
+        applicationId = "com.github.acredick.dormdash"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23  // Update this to 23
@@ -41,6 +41,8 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+
+    namespace = "com.github.acredick.dormdash"
 
     signingConfigs {
         create("release") {
